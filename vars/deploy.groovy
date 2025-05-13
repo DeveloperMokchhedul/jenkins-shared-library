ea1040c1df68a737a342b0fe66850052c54e4c30
mokchhedul islam <mokchheduls46@gmail.com>
2025-05-13 13:44:02 +0500

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,7 +2,7 @@ def call(String containername, String imagename, String imagetag){
   echo "deploy start"
   sh """
   docker rm -f ${containername} || true
-  docker run -d --name ${containername} -p 80:3000 ${imagename}:${imagetag}
+  docker run -d --name ${containername} -p 80:5000 ${imagename}:${imagetag}
   """
   echo "deployment complete"
 }
