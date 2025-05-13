7e0f6cc6a4e41c9c29d41e36e37aeb05877dc834
mokchhedul islam <mokchheduls46@gmail.com>
2025-05-13 13:02:20 +0500

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 def call(String imagename, String imagetag){
   echo "docker build"
-  sh "docker build -t ${imagename}:${imagetag}"
+  sh "docker build -t ${imagename}:${imagetag} ."
   echo "docker build complate"
 
   
