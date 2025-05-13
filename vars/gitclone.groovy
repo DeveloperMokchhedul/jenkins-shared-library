6342a7383d23e1f9b9479636ab45d4fde3fd4f38
mokchhedul islam <mokchheduls46@gmail.com>
2025-05-13 12:36:56 +0500

    Update gitclone.groovy

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -1,4 +1,4 @@
-def call(string url, string branch){
+def call(){
   echo "hey this is cloning git code"
   git url: "https://github.com/DeveloperMokchhedul/auto_deployment_with_aws.git", branch: "main"
   echo "complate cloning code"
