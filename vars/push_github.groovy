2cb71fc91a12c968e2186ac49b6d50e445e3a56f
mokchhedul islam <mokchheduls46@gmail.com>
2025-05-25 12:18:25 +0500

    Update push_github.groovy

--- vars/push_github.groovy
+++ vars/push_github.groovy
@@ -1,4 +1,4 @@
-def cal(String username, String imagename, String tagname){
+def call(String username, String imagename, String tagname){
         echo 'Pushing the image to Docker Hub'
         withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
         sh 'docker login -u $dockerHubUser -p $dockerHubPassword'
